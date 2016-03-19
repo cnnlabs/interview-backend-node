fe7acb369f3cd53aa1b561d02834e646ffaf4399
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-03-19 17:12:13 -0400

    (test) Complete instruction's reqs for extra credit testing.

--- test/test.js
+++ test/test.js
@@ -1,25 +1,57 @@
 'use strict';
 
+/*
+ * ### Assumptions:
+ *
+ * - That the instruction's "Write a unit test" did not really mean a single text.
+ */
+
 let expect = require('chai').expect,
+
     main = require('../main');
 
 
-describe('JSON output', function () {
-    let out;
+describe('main.js\'s JSON output', function () {
+    let out,
+        outCache;
 
-    beforeEach(function (done) {
+    before(function (done) {
         main(function (newFeed) {
-          out = newFeed;
-          done();
-        })
+            outCache = newFeed;
+            done();
+        });
+    });
+
+    beforeEach(function () {
+        out = outCache;
     });
 
     it('is a valid JSON string', function () {
-      expect(JSON.parse(out)).to.be.ok;
+        expect(JSON.parse(out)).to.be.ok;
     });
 
-    //   // representing array of article objects
-    //   // from the "Top stories" container
-    //   // each article has following, and only following, properties,
-    //   // each article property is typeof String
+    it('represents an array', function () {
+        let outObj = JSON.parse(out);
+        expect(Array.isArray(outObj));
+    });
+
+    it('represents an array of objects', function () {
+        let outObj = JSON.parse(out);
+        outObj.every(function (elt) {
+            return elt.constructor === Object;
+        });
+    });
+
+    describe('represents an array of article objects that', function () {
+        it('have following, and only following, properties of type string', function () {
+            let outObj = JSON.parse(out);
+            outObj.every(function (article) {
+                expect(Object.keys(article)).to.have.length(4);
+                expect(article).to.have.property('url').that.is.a('string');
+                expect(article).to.have.property('headline').that.is.a('string');
+                expect(article).to.have.property('imageUrl').that.is.a('string');
+                expect(article).to.have.property('byLine').that.is.a('string');
+            });
+        });
+    });
 });
