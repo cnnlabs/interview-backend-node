6fa0cbb0ee873fa06503d7a2e7a9566885df8305
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-03-19 14:39:54 -0400

    (refactor) Clean up #getHighestQualityCutUri

--- scripts/transform_article.js
+++ scripts/transform_article.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // TODO doc
 const DOMAIN = 'www.cnn.com',
     URL_PROTOCOL = 'http://';
@@ -29,24 +31,18 @@ function genUrl(article) {
     }
 }
 function getHighestQualityCutUri(cuts) {
-    if (cuts.full16x9 && cuts.full16x9.uri) {
-        return cuts.full16x9.uri;
-    } else if (cuts.large && cuts.large.uri) {
-        return cuts.large.uri;
-    } else if (cuts.medium && cuts.medium.uri) {
-        return cuts.medium.uri;
-    } else if (cuts.small && cuts.small.uri) {
-        return cuts.small.uri;
-    } else if (cuts.xsmall && cuts.xsmall.uri) {
-        return cuts.xsmall.uri;
-    } else if (cuts.mini && cuts.mini.uri) {
-        return cuts.mini.uri;
     // NOTE: mini1x1 is a special cut that is not always in the same aspect ratio
-    } else if (cuts.mini1x1 && cuts.mini1x1.uri) {
-        return cuts.mini1x1.uri;
-    } else {
+    let sizes = ['full16x9', 'large', 'medium', 'small', 'xsmall', 'mini', 'mini1x1'],
+        highestQualityCut = '';
+
+    highestQualityCut = sizes.find(function (size) {
+        return cuts[size] && cuts[size].uri;
+    });
+    if (!highestQualityCut) {
         return '';
     }
+
+    return cuts[highestQualityCut].uri;
 }
 
 
