76b6434e18ac9dfeabb358f4834e1e381d094f79
Jeff Carter <jcarter00@gmail.com>
2016-04-21 13:27:06 -0400

    fix indentation and spacing

--- transform-top-stories.js
+++ transform-top-stories.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports.transformTopStories = function (topStories) {
-  var url = 'http://www.cnn.com',
-      outputTopStories = [];
+    var url = 'http://www.cnn.com',
+        outputTopStories = [];
 
-  topStories.forEach(function (topStory) {
-    var output = {};
+    topStories.forEach(function (topStory) {
+        var output = {};
         output.url      = url + topStory.cardContents.url;
         output.headline = topStory.cardContents.headlinePlainText;
         output.imageUrl = topStory.cardContents.media.elementContens.imageUrl;
         output.byLine   = topStory.cardContents.auxiliaryText;
         outputTopStories.push(output);
-  });
-  console.log('Top Stories: ', outputTopStories);
-  return outputTopStories;
+    });
+    console.log('Top Stories: ', outputTopStories);
+    return outputTopStories;
 };
