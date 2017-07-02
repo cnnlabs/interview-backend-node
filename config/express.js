c0e66e8ea9f649efe3d88f88c176798b1571c450
Jenny Vallon <jennyvallon@gmail.com>
2017-07-02 11:07:23 -0400

    removed swagger

--- config/express.js
+++ config/express.js
@@ -1,6 +1,5 @@
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    swagger=require('swagger-express');
+    bodyParser = require('body-parser');
 
 module.exports = function () {
     var app = express();
@@ -10,20 +9,6 @@ module.exports = function () {
     app.use(bodyParser.json());
 
     require('../app/routes/app.server.routes.js')(app);
-
-    app.use(express.static('./public'));
-
-    app.use(swagger.init(app, {
-        apiVersion: '1.0',
-        swaggerVersion: '1.0',
-        swaggerURL: '/swagger',
-        swaggerJSON: '/api-docs.json',
-        swaggerUI: './public/swagger/',
-        basePath: 'http://localhost:3000',
-        apis: ['./api.js', './api.yml'],
-        middleware: function(req, res){}
-  }));
-
 
     return app;
 };
